6c25e7abf39e74e22724c7958e4724fad2281170
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-10-10 00:07:15 -0700

    add basic png write out code

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 int main(int argc, char **argv) {
   const int image_width = 256;
   const int image_height = 256;
+  const int num_channels = 3;
 
-  printf("P3\n");
-  printf("%d %d\n", image_width, image_height);
-  printf("255\n");
+  uint8_t image[image_width * image_height * num_channels];
 
   for (int i = image_height - 1; i >= 0; i -= 1) {
     for (int j = 0; j < image_width; j += 1) {
@@ -14,13 +17,18 @@ int main(int argc, char **argv) {
       double g = (double)i / (double)(image_height - 1);
       double b = 0.25;
 
-      int ir = (int)(r * 255.999);
-      int ig = (int)(g * 255.999);
-      int ib = (int)(b * 255.999);
+      uint8_t ir = (int)(r * 255.999);
+      uint8_t ig = (int)(g * 255.999);
+      uint8_t ib = (int)(b * 255.999);
 
-      printf("%d %d %d\n", ir, ig, ib);
+      int offset = (image_height * (image_height - i - 1) + j) * num_channels;
+      image[offset] = ir;
+      image[offset + 1] = ig;
+      image[offset + 2] = ib;
     }
   }
+
+  stbi_write_png("out.png", image_width, image_height, num_channels, image, image_width * num_channels);
 
   return 0;
 }
