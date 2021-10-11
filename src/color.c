65fc8199ded7d033c491cb51a953c8b1410bd77b
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-10-10 17:44:36 -0700

    add ray struct and impl

--- src/color.c
+++ src/color.c
@@ -1,6 +1,6 @@
 #include "color.h"
 
-void vec3_to_color(const vec3 color, uint8_t *r, uint8_t *g, uint8_t *b) {
+void vec3_to_color(const Vec3 color, uint8_t *r, uint8_t *g, uint8_t *b) {
   *r = (int)(255.999 * color.x);
   *g = (int)(255.999 * color.y);
   *b = (int)(255.999 * color.z);
--- src/ray.c
+++ src/ray.c
@@ -1,6 +1,6 @@
 #include "ray.h"
 #include "vec3.h"
 
-point3 ray_at(Ray ray, double t) {
+Point3 ray_at(Ray ray, double t) {
   return vec3_add(ray.orig, vec3_mul_scalar(ray.dir, t));
 }
