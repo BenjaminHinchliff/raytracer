c2b69f9339d852e4eac2a12cd21680b027d7b9e3
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-10-12 08:42:44 -0700

    add cplusplus guards to util.h

--- include/util.h
+++ include/util.h
@@ -1,6 +1,10 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdlib.h>
 
 #ifndef M_PI
@@ -11,5 +15,9 @@ double degrees_to_radians(double degrees);
 
 double random_double();
 double random_double_range(double min, double max);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
