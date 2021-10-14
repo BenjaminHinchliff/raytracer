bf83845d5a5a1a523fa22a5ff4908c96a668bc54
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-10-14 14:31:59 -0700

    refactor material delegation

--- src/material.c
+++ src/material.c
@@ -5,21 +5,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-bool material_scatter(const Material *material, const Ray *ray,
-                      const struct HitRecord rec, Color *attenuation,
-                      Ray *scattered) {
+typedef bool (*scatter_fn)(const Material *material, const Ray *ray,
+                           const struct HitRecord rec, Color *attenuation,
+                           Ray *scattered);
+
+bool scatter_lambertian(const Material *material, const Ray *ray,
+                        const struct HitRecord rec, Color *attenuation,
+                        Ray *scattered) {
   Vec3 scatter_direction = rec.normal;
   Vec3 random_unit = vec3_random_unit_sphere();
   vec3_add(&scatter_direction, &random_unit);
   *scattered = ray_new(rec.p, scatter_direction);
-  // TODO: make this less messy
-  switch (material->type) {
-  case MATERIAL_TYPE_lambertian:
-    *attenuation = material->albedo;
-    break;
-  default:
-    fprintf(stderr, "INVALID MATERIAL TYPE IN material_scatter!\n");
-    exit(1);
-  }
+  *attenuation = material->albedo;
+
+  return true;
+}
+
+bool scatter_panic(const Material *material, const Ray *ray,
+                   const struct HitRecord rec, Color *attenuation,
+                   Ray *scattered) {
+  fprintf(stderr, "ILLEGAL TYPE CALL IN get_scatter_action\n");
+  exit(1);
+}
+
+scatter_fn get_scatter_action(enum MATERIAL_TYPE type) {
+  return (type == MATERIAL_TYPE_lambertian) ? scatter_lambertian
+                                            : scatter_panic;
+}
+
+bool material_scatter(const Material *material, const Ray *ray,
+                      const struct HitRecord rec, Color *attenuation,
+                      Ray *scattered) {
+  get_scatter_action(material->type)(material, ray, rec, attenuation,
+                                     scattered);
   return true;
 }
